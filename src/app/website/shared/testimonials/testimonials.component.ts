8aab5d4e52744767ef31fa97426a2c306a28e263
bhooshan kalamani <bhooshan.kalamani@hebbalelabs.com>
2018-01-03 16:06:41 +0530

    added changes to testimonial component

--- src/app/website/shared/testimonials/testimonials.component.ts
+++ src/app/website/shared/testimonials/testimonials.component.ts
@@ -9,64 +9,46 @@ import { ContentfulService } from '../contentful/contentful.service';
   styleUrls: ['./testimonials.component.scss']
 })
 export class TestimonialsComponent implements OnInit {
-  @Input() page = '';
-  contentData = {};
+  @Input() page: string = '';
+  contentData: any = {};
   // The time to show the next photo
   private nextPhotoInterval: number = 5000;
   // Looping or not
   private noLoopSlides: boolean = false;
-
-
   constructor(private http: Http,
     private contentfulService: ContentfulService) { }
 
   ngOnInit() {
     this.getContents(this.page);
-
   }
-
   getContents(page) {
     this.contentfulService.getTestimonialData()
       .subscribe(
       (data: any) => {
-
         for (let item of data.items) {
-
           if (item.fields.pageName === page) {
             this.contentData = getValue(item);
           }
-
         }
         function getValue(item) {
-          let sectionHeading = item.fields.sectionTitle;
-          let testimonials = [];
+          let sectionHeading: string = item.fields.sectionTitle;
+          let testimonials: any = [];
           for (let entryRefernce of item.fields.testimonials) {
             testimonials.push(getDetails(entryRefernce));
           }
           function getDetails(entryRefernce) {
-            let entryValue = data.includes.Entry.find(getEntry);
-            let assetValue = data.includes.Asset.find(getAsset);
-
-
+            let entryValue: any = data.includes.Entry.find(getEntry);
+            let assetValue: any = data.includes.Asset.find(getAsset);
             function getEntry(entry) {
               return entry.sys.id === entryRefernce.sys.id;
             }
             function getAsset(asset) {
-              console.log(entryValue);
               return asset.sys.id === entryValue.fields.profilePhoto.sys.id;
             }
             return { entryValue, assetValue };
-
           }
           return { sectionHeading, testimonials };
         }
-
-        console.log(this.contentData);
-      }
-
-      );
-
-
+      });
   }
-
 }
