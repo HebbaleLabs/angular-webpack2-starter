6aa808435e3631d9430b80445a187dd494b40337
bhooshan kalamani <bhooshan.kalamani@hebbalelabs.com>
2018-01-04 16:40:04 +0530

    removed unwanted imports from course home page files

--- src/app/website/course-home-page/popular-courses/popular-courses.component.ts
+++ src/app/website/course-home-page/popular-courses/popular-courses.component.ts
@@ -1,15 +1,11 @@
 import { PopularCourseCardDisplayModel } from './../course-home-page.model';
-import { Component, OnInit, Input } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { ContentfulService } from '../../shared/contentful/contentful.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tf-popular-courses',
   templateUrl: './popular-courses.component.html',
   styleUrls: ['./popular-courses.component.scss']
 })
-export class PopularCoursesComponent implements OnInit {
+export class PopularCoursesComponent {
   @Input() displayModel: PopularCourseCardDisplayModel;
-  ngOnInit() {
-  }
 }
