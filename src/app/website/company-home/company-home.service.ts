47a1122dd3ec7e8f14492f2adf2f35431db1854c
bhooshan kalamani <bhooshan.kalamani@hebbalelabs.com>
2018-01-04 20:19:30 +0530

    implemented model for featuresummary component in company home page

--- src/app/website/company-home/company-home.service.ts
+++ src/app/website/company-home/company-home.service.ts
@@ -1,4 +1,4 @@
-import { ManyPossibilitiesModel } from './company-home.model';
+import { ManyPossibilitiesModel, FeatureSummaryDataModel } from './company-home.model';
 import { Injectable } from '@angular/core';
 @Injectable()
 export class CompanyHomeService {
@@ -13,5 +13,38 @@ export class CompanyHomeService {
       possibiliteis.push(dataModel);
     });
     return { sectionTitle, possibiliteis };
+  }
+  getFeatureSummary(data: any): FeatureSummaryDataModel {
+    const advantagedataModelMap = {};
+    const imageModelMap = {};
+    const featuresSummarySections: any[] = [];
+
+    data.items.forEach(item => {
+      const displaydataModel = {
+        sectionTitle: item.fields.sectionHeader,
+        sectionDescription: item.fields.sectionDescription,
+        advantages: [],
+        image: {}
+      };
+      console.log(displaydataModel);
+      featuresSummarySections.push(displaydataModel);
+
+      imageModelMap[item.fields.teaserImage.sys.id] = displaydataModel;
+      item.fields.advantages.forEach(advantage => {
+        advantagedataModelMap[advantage.sys.id] = displaydataModel;
+      });
+    });
+    data.includes.Entry.forEach(entry => {
+      const displaydataModel = advantagedataModelMap[entry.sys.id];
+      const advantageData = {
+        title: entry.fields.advantage,
+        icon: entry.fields.cssFaIcon
+      };
+      displaydataModel.advantages.push(advantageData);
+    });
+    data.includes.Asset.forEach(asset => {
+      imageModelMap[asset.sys.id].image = asset.fields.file.url;
+    });
+    return { featuresSummarySections };
   }
 }
--- src/app/website/company-home/feature-summary/feature-summary.component.ts
+++ src/app/website/company-home/feature-summary/feature-summary.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
-import { ContentfulService } from '../../shared/contentful/contentful.service';
+import { FeatureSummaryDataModel } from '../company-home.model';
+import { Component, Input } from '@angular/core';
+
 
 
 @Component({
@@ -10,56 +8,7 @@ import { ContentfulService } from '../../shared/contentful/contentful.service';
   templateUrl: './feature-summary.component.html',
   styleUrls: ['./feature-summary.component.scss']
 })
-export class FeatureSummaryComponent implements OnInit {
-
-  contentData: any = [];
-  constructor(private http: Http,
-    private contentfulService: ContentfulService) { }
-
-
-
-  ngOnInit() {
-    this.getContents();
-  }
-
-  getContents() {
-    this.contentfulService.getFeatureSummaryData()
-      .subscribe(
-      (data: any) => {
-        for (const item of data.items) {
-          this.contentData.push(getValue(item));
-
-        }
-
-        function getValue(item) {
-          const sectionHeading = item.fields.sectionHeader;
-          const sectiDescription = item.fields.sectionDescription;
-          const advantages: any = [];
-          const assetValue = data.includes.Asset.find(getAsset);
-          function getAsset(asset) {
-
-            return asset.sys.id === item.fields.teaserImage.sys.id;
-
-          }
-          for (const advantage of item.fields.advantages) {
-            advantages.push(getEntry(advantage));
-          }
-          function getEntry(advantage) {
-
-            function getItem(entry) {
-              return entry.sys.id === advantage.sys.id;
-
-            }
-            return data.includes.Entry.find(getItem);
-
-          }
-
-
-          return { sectionHeading, sectiDescription, advantages, assetValue };
-        }
-
-      }
-      );
-
-  }
+export class FeatureSummaryComponent {
+  @Input() displayModel: FeatureSummaryDataModel;
 }
+
