28ac84de89ba0313c0e6f3f03b52dd7a21655bc9
bhooshan kalamani <bhooshan.kalamani@hebbalelabs.com>
2018-01-04 16:40:04 +0530

    removed unwanted imports from course home page files

--- src/app/website/course-home-page/course-home-page.component.ts
+++ src/app/website/course-home-page/course-home-page.component.ts
@@ -2,9 +2,6 @@ import { CourseHomePageService } from './course-home-page.service';
 import { PopularCourseCardDisplayModel, WhyLearnDisplayModel } from './course-home-page.model';
 import { ContentfulService } from './../shared/contentful/contentful.service';
 import { Component, OnInit } from '@angular/core';
-import { Http, Response } from '@angular/http';
-
-
 
 @Component({
   selector: 'tf-course-home-page',
@@ -19,8 +16,8 @@ export class CourseHomePageComponent implements OnInit {
 
   popularCourseCardDisplayModel: PopularCourseCardDisplayModel;
   whyLearnDisplayModel: WhyLearnDisplayModel;
-  constructor(private http: Http,
-    private contentfulService: ContentfulService,
+
+  constructor(private contentfulService: ContentfulService,
     private courseHomePageService: CourseHomePageService) { }
   ngOnInit() {
     this.getPopularCourseCards();
--- src/app/website/course-home-page/why-learn/why-learn.component.ts
+++ src/app/website/course-home-page/why-learn/why-learn.component.ts
@@ -1,8 +1,5 @@
 import { WhyLearnDisplayModel } from './../course-home-page.model';
 import { Component, Input } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/Rx';
-import { ContentfulService } from '../../shared/contentful/contentful.service';
 
 @Component({
   selector: 'tf-why-learn',
